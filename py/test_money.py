b6ec40308c7a3c37fc33de5b829355d99cc48639
mrfz <zlobin.f@gmail.com>
2022-09-01 17:40:06 +0300

    feat improved error handling for missingexchangee rates

--- py/test_money.py
+++ py/test_money.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import unittest
 
 from money import Money
@@ -40,6 +41,18 @@ class TestMoney(unittest.TestCase):
         expectedValue = Money(2200, "KRW")
         actualValue = portfolio.evaluate("KRW")
         self.assertEqual(expectedValue, actualValue, "%s != %s" % (expectedValue, actualValue))
+
+    def testAdditionWithMultipleMissingExchangeRates(self):
+        oneDollar = Money(1, "USD")
+        oneEuro = Money(1, "EUR")
+        oneWon = Money(1, "KRW")
+        portfolio = Portfolio()
+        portfolio.add(oneDollar, oneEuro, oneWon)
+        with self.assertRaisesRegex(
+            Exception,
+            "Missing exchange rate\(s\):\[USD\->Kalganid,EUR->Kalganid,KRW->Kalganid]",
+        ):
+            portfolio.evaluate("Kalganid")
 
 if __name__ == '__main__':
 
